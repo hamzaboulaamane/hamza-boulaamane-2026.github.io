8fa92d75e52970f69965c869df2fff9220b5bb54
hamzaboulaamane <hamzaboulaamane06@gmail.com>
2026-02-10 18:53:27 +0000

    Initial commit

---
+++ files/script.js
@@ -0,0 +1,56 @@
+const commandInput = document.getElementById("command");
+const output = document.getElementById("output");
+
+commandInput.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    const cmd = commandInput.value.toLowerCase();
+    output.innerHTML += `<p><span class="prompt">hamza@portfolio:~$</span> ${cmd}</p>`;
+    handleCommand(cmd);
+    commandInput.value = "";
+    output.scrollTop = output.scrollHeight;
+  }
+});
+
+function handleCommand(cmd) {
+  switch (cmd) {
+    case "help":
+      print(`
+Available commands:
+about | skills | projects | education | contact | clear
+`);
+      break;
+
+    case "about":
+      print("I’m Hamza, a computer science student passionate about AI & cybersecurity.");
+      break;
+
+    case "skills":
+      print("Python, C, HTML, CSS, JavaScript, Linux, Networking, ML basics,UML");
+      break;
+
+    case "projects":
+      print("• Spam email classifier\n• Portfolio website\n• Natural products app");
+      break;
+
+    case "education":
+      print("BSc Computer Science & Mathematical Sciences");
+      break;
+
+    case "contact":
+      print("Email: hamzaboulaamane06@email.com\nGitHub: github.com/hamza boulaamane");
+      break;
+
+    case "clear":
+      output.innerHTML = "";
+      break;
+
+    default:
+      print(`Command not found: ${cmd}`);
+  }
+}
+
+function print(text) {
+  text.split("\n").forEach(line => {
+    output.innerHTML += `<p>${line}</p>`;
+  });
+}
